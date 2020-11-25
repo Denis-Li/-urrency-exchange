0eb13c9a2df0b62b8d9c0ca4b265ca9a60fdb244
Denis-Li <den.le.genplan@gmail.com>
2020-11-25 13:00:14 +0100

    Updates

---
+++ precache-manifest.8552645ae5a8fcbb95fa9f170708a327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15669d994576ac12b3273963e04f185",
+    "url": "/currency-exchange/index.html"
+  },
+  {
+    "revision": "df35cb5b62df0f35c33d",
+    "url": "/currency-exchange/static/css/main.73a6f978.chunk.css"
+  },
+  {
+    "revision": "4eb15a80a44af675903e",
+    "url": "/currency-exchange/static/js/2.15a30bd6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/currency-exchange/static/js/2.15a30bd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df35cb5b62df0f35c33d",
+    "url": "/currency-exchange/static/js/main.db654b2a.chunk.js"
+  },
+  {
+    "revision": "ced8dba84bb033c9dc69",
+    "url": "/currency-exchange/static/js/runtime-main.daf59f97.js"
+  }
+]);
